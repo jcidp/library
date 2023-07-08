901fff15de087e5d6e6b3619e7620c8d8eecd08d
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-07-07 19:26:39 -0600

    Add Book constructor, library, and function that adds book to library

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read}`
+    };
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayLibrary(library) {
+
+}
